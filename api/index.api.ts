68bc3bf74cb4bbb717eef851d7dda0efb58bbb1e
KyeongYeongDEV <yeong7098@naver.com>
2024-09-21 18:25:21 +0900

    init

---
+++ api/index.api.ts
@@ -0,0 +1,12 @@
+import Router from "express";
+// import auth from "./routes/auth.route";
+// import chat from "./routes/chat.route";
+
+export default () => {
+    const router= Router();
+    
+    // auth({ app : router });
+    // chat({ app : router });
+
+    return router;
+}
---
+++ loaders/express.loader.ts
@@ -0,0 +1,24 @@
+import { Application, json, urlencoded } from "express";
+import router from '../api/index.api';
+//import errorMiddleware from "../api/middlewares/error.middleware";
+import cors from "cors";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default async ({ app }: { app: Application }) => {
+    app.use(cors((req, callback) => {
+        console.log(`CORS enabled for: ${req.method} ${req.url}`);
+        callback(null, { origin: true });
+    }));
+    app.use(json());
+    app.use(urlencoded({ extended: false }));
+    app.use('/node', router());
+
+    app.get('/', (req, res) => {
+        res.send('<h1>Hello</h1>')
+    });
+
+    // app.use(errorMiddleware);
+};
